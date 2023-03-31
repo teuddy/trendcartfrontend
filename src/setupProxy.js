28f8bd0c17920a97a56a09873f9710c11bd58412
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-03-31 19:05:25 -0400

    chanegorigin

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 
+
+//work here to properly have workstation and production
+
 module.exports = function (app) {
   app.use(
     "/api",
@@ -8,7 +11,7 @@ module.exports = function (app) {
       target: `https://backend.trendcarts.net/v1`,
       // target: process.env.ENV == 'production' ? `https://backend.trendcarts.net/v1` : `http://${process.env.HOST}:${process.env.PORT}/v1`,
       pathRewrite: { "^/api": "" },
-      changeOrigin:true
+      // changeOrigin:true
       // changeOrigin: process.env.ENV == 'production' ? true : false,
     })
   );
