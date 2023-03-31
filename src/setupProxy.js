a1f2f2ba3682cd1fd2295e91b039b588c667df97
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-03-31 19:10:29 -0400

    change origin false

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
       target: `https://backend.trendcarts.net/v1`,
       // target: process.env.ENV == 'production' ? `https://backend.trendcarts.net/v1` : `http://${process.env.HOST}:${process.env.PORT}/v1`,
       pathRewrite: { "^/api": "" },
-      // changeOrigin:true
+      changeOrigin:false
       // changeOrigin: process.env.ENV == 'production' ? true : false,
     })
   );
