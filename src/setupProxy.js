da1e58e2e313a05eeccb6f90f695892766156dd1
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-03-31 19:18:43 -0400

    dd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,36 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-
-
-//work here to properly have workstation and production
-
 module.exports = function (app) {
+  const isProduction = 'production' === 'production';
+
   app.use(
     "/api",
     createProxyMiddleware({
-      target: `https://backend.trendcarts.net/v1`,
-      // target: process.env.ENV == 'production' ? `https://backend.trendcarts.net/v1` : `http://${process.env.HOST}:${process.env.PORT}/v1`,
+      target: isProduction
+        ? 'https://backend.trendcarts.net/v1'
+        : 'http://localhost:8080/v1',
       pathRewrite: { "^/api": "" },
-      changeOrigin:false
-      // changeOrigin: process.env.ENV == 'production' ? true : false,
+      changeOrigin: isProduction,
     })
   );
 };
+
+
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+
+
+// //work here to properly have workstation and production
+
+// module.exports = function (app) {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: `https://backend.trendcarts.net/v1`,
+//       // target: process.env.ENV == 'production' ? `https://backend.trendcarts.net/v1` : `http://${process.env.HOST}:${process.env.PORT}/v1`,
+//       pathRewrite: { "^/api": "" },
+//       changeOrigin:false
+//       // changeOrigin: process.env.ENV == 'production' ? true : false,
+//     })
+//   );
+// };
